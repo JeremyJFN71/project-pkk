a13b357f95fbbe4a83a7f7f56c953d011b1d4d23
Iyan Siagian <iyansiagian04@gmail.com>
2023-03-06 13:32:06 +0700

    admin

--- app/Controllers/Http/admin/AdminsController.ts
+++ app/Controllers/Http/admin/AdminsController.ts
@@ -1,9 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Product from 'App/Models/Product'
 
 export default class AdminsController {
     public async index({ view }:HttpContextContract){
+        const products = await Product.query().preload('images')
         return view.render('admin/admin', {
-            title: 'Admin'
+            title: 'Admin',
+            products
         })
     }
 }
