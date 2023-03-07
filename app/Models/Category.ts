63400ae39b6702527fd2773729aafda84c90130c
JeremyJFN71 <jeremyjeferson71@gmail.com>
2023-03-07 08:23:39 +0700

    add category table

---
+++ app/Models/Category.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import Product from './Product'
+
+export default class Category extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public slug: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @hasMany(() => Product, {
+    foreignKey: 'category'
+  })
+  public products: HasMany<typeof Product>
+}
--- database/seeders/ProductSeeder.ts
+++ database/seeders/ProductSeeder.ts
@@ -1,11 +1,27 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Product from 'App/Models/Product'
 import ProductImage from 'App/Models/ProductImage'
+import Category from 'App/Models/Category'
 
 export default class extends BaseSeeder {
   public async run () {
-    // Write your database queries inside the run method
+    // Category
+    Category.createMany([
+      {
+        name: 'Baju',
+        slug: 'baju'
+      },
+      {
+        name: 'Tas',
+        slug: 'tas'
+      },
+      {
+        name: 'Jaket',
+        slug: 'jaket'
+      },
+    ])
 
+    // Product
     await Product.createMany([
       {
         name: 'Anari Girls',
@@ -13,37 +29,42 @@ export default class extends BaseSeeder {
 - Ukuran: 35cm x 39 cm
 - Bahan: Kanvas
 - Warna: Broken White
-- Cocok dibawa ke sekolah dan kantor karena ukurannya yang tidak terlalu besar dan terlalu kecil
-`,
+- Cocok dibawa ke sekolah dan kantor karena ukurannya yang tidak terlalu besar dan terlalu kecil`,
+        category_id: 2,
         wa_number: '085715530645',
         price: 12000
       },
       {
         name: 'Cute Astronaut T-Shirt',
         description: `T-Shirt Original by camil's collection, berbahan Cotton Combed 30s High Quality. Tersedia berbagai pilihan warna, ukuran (S, M, L, XL).`,
+        category_id: 1,
         wa_number: '081291125062',
         price: 30000
       },
       {
         name: 'SIJA AIThreads Black T-Shirt',
         description: `Kaos tema AI Threads adalah kaos dengan desain yang terinspirasi oleh perkembangan teknologi kecerdasan buatan (AI) dan Sistem Infromasi Jaringan dan Aplikasi(SIJA). Kaos ini menggunakan gambar atau grafik yang menunjukkan berbagai penjelasan tentang Kelas XII SIJA 2 dan teknologi AI.`,
+        category_id: 1,
         wa_number: '085717919259',
         price: 89999
       },
       {
         name: "O'Sweats",
         description: `Sweater yang terbuat dari bahan babyterry berkualitas. Tersedia berbagai banyak ukuran, mulai dari ukuran S, M, L, XL. Bahan yang digunakan adem. Tersedia berbagai varian warna, seperti abu-abu, merah, dan hitam. Dapat request inisial nama, dan gambarnya juga bisa kustom lho.`,
+        category_id: 3,
         wa_number: '088212701317',
         price: 50000
       },
       {
         name: 'Tote bag',
         description: `Tas jinjing (totebag) bernama Lafeyo, yang diambil dari ketiga nama anggota kelompok, memiliki bahan serat kanvas, dengan desain yang simple dan kekinian. </3`,
+        category_id: 2,
         wa_number: '081291125070',
         price: 49900
       },
     ])
 
+    // Product Image
     await ProductImage.createMany([
       {
         image: '/uploads/Picsart_23-03-03_21-17-08-347 - Rini_rahmawati.png',
